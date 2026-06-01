5cf63726bbc7384018734b422e9768a8bd1077e3
Neeraj kumar <neeraj772006@gmail.com>
2026-06-01 16:26:27 +0530

    phase 2 of frequently asked questions

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let right_btn = document.querySelector("#right_btn");
 
 let movie_row = document.querySelector("#movie_row");
 
-console.dir(movie_row);
-
 
 left_btn.addEventListener("click" , function(){
     movie_row.scrollTo({
@@ -17,5 +15,23 @@ right_btn.addEventListener("click" , function(){
     movie_row.scrollTo({
         left: movie_row.scrollWidth,
         behavior : "smooth"
+    });
+});
+
+
+// answers to frequently asked questions
+
+
+let questions = document.querySelectorAll(".question");
+questions.forEach((x , y) => {
+    x.addEventListener("click" , function(z){ 
+        document.querySelector(`#ans${y+1}`).style.display = "inline";
+        x.querySelector("span").style.transform = "rotate(45deg)";
+        for(var i = 0 ; i < questions.length ;i++){
+            if(i != y) {
+                document.querySelector(`#ans${i+1}`).style.display = "none";
+                // document.querySelector(`#ans${i+1}`).style.
+            }
+        }
     });
 });
