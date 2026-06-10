450139503ca31df3df36f5ab2b427c1813d79f71
Neeraj kumar <neeraj772006@gmail.com>
2026-06-10 13:38:33 +0530

    added wrong email alert

--- script.js
+++ script.js
@@ -35,3 +35,43 @@ questions.forEach((x , y) => {
         }
     });
 });
+
+
+
+
+//email id get started .-------------------------------------------------
+
+let get_started1 = document.querySelector("#get_started1");
+
+get_started1.addEventListener("click" , function(){
+    event.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    let email1 = document.querySelector("#email_1");
+    console.dir(email1);
+    if(!emailRegex.test(email1.value)){
+        document.querySelector("#wrong_alert_1").removeAttribute("hidden");
+    }
+    else{
+        document.querySelector("#wrong_alert_1").setAttribute("hidden","");
+    }
+});
+
+
+
+let get_started_2 = document.querySelector("#get_started2");
+get_started_2.addEventListener("click" , function(){
+    event.preventDefault();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    let email2 = document.querySelector("#email_2");
+    if(emailRegex.test(email2.value)){
+        document.querySelector("#wrong_alert_2").setAttribute("hidden","");
+    }
+    else{
+        document.querySelector("#wrong_alert_2").removeAttribute("hidden");
+    }
+});
+
+//-------------------------------------------------------------------
